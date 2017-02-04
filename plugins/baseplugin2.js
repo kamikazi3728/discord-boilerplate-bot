3f09261930fef07b014db7f243e989d44e55d696
kamikazi3728 <reeceappling@gmail.com>
2017-02-04 01:48:24 -0500

    implemented voice channel sorting in ready event for baseplugin2

--- plugins/baseplugin2.js
+++ plugins/baseplugin2.js
@@ -1,6 +1,34 @@
 module.exports = function(client,args) {
 
   client.on('ready', ()=>{
-	  console.log(args[0]+ client.user.username + args[1]);
+	console.log('[' + new Date().toISOString() + '] Connected!');
+	
+	// Set the presence status.
+	client.user.setStatus('online');
+	
+	// Get a list of channels.
+	var channelsOrdered = client.channels.array().slice(0);
+	
+	// Evaluate only voice channels.
+	channelsOrdered = channelsOrdered.filter(function(channel) {
+		return channel.type == 'voice' && typeof channel.position !== 'undefined';
+	});
+	
+	// Sort channels by their current position.
+	channelsOrdered = channelsOrdered.sort(function(channelA, channelB) {
+		return channelA.position - channelB.position;
+	});
+	
+	// Re-sort channels to support auto-grouping and maximum voice quality.
+	var currentPosition = 100;
+	channelsOrdered.forEach(function(channel) {
+		currentChannel = client.channels.get(channel.id);
+		currentChannel.edit({bitrate: 96000, position: currentPosition})
+			.then(editedChannel => {
+				console.log('[' + new Date().toISOString() + '] Set ' + editedChannel.type + ' channel "' + editedChannel.name + '" (' + editedChannel.id + ') position to ' + editedChannel.position + ' with ' + editedChannel.bitrate / 1000 + 'kbps bitrate')
+			})
+			.catch(console.error);
+		currentPosition += 100;
+	});
   });
 }
