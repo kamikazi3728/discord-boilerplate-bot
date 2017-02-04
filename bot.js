61c50c7d28b865fd4797b2c2dec2e1af65028878
kamikazi3728 <reeceappling@gmail.com>
2017-02-04 01:30:02 -0500

    added missing semicolon, oops

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-client.setMaxListeners(41)
+client.setMaxListeners(41);
 const config = require('./config.json'); // require configuration file
 
 for (var p=0; p<config['plugins'].length; p++){//require all plugin scripts with client and configuration arguments
